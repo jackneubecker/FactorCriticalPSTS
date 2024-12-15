f64670902991f2741a8961ccdd7e13b8706ade28
= <JackNeubecker.13@gmail.com>
2024-12-15 14:53:47 +1000

    half way through 34. Starting on the last new type of construction.

---
+++ 31/PSTS_31_62_155/checker.py
@@ -0,0 +1,31 @@
+from helper_methods import * 
+import importlib
+import sys 
+
+importlib.reload(sys.modules['helper_methods'])
+
+n = 31
+point_set = list(range(n))
+
+starter_triples = txt_to_list_of_triples('31/PSTS_31_62_155/data/starter_triples.txt',str_to_int=True)
+
+triples = []
+for i in range(n): 
+    for T in starter_triples: 
+        triples.append(sorted([(T[j]+i)%n for j in range(len(T))]))
+
+is_PSTS_checker(triples,point_set = list(range(n)))
+
+APCs = txt_to_dict_of_APCs('31/PSTS_31_62_155/data/APCs.txt', str_to_int=True)
+
+is_APCs_checker(APCs, list(range(n)))
+has_APCs_checker(triples, APCs)
+
+extra_starter_triples = txt_to_list_of_triples('31/PSTS_31_62_155/data/extra_starter_triples.txt',str_to_int=True)
+
+extra_triples = []
+for i in range(n): 
+    for T in extra_starter_triples: 
+        extra_triples.append(sorted([(T[j]+i)%n for j in range(len(T))]))
+
+extra_triples_checker(triples, extra_triples, point_set)
