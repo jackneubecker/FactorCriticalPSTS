e9470450b00f35818596c6d12a84ef6bfb2f6a15
= <JackNeubecker.13@gmail.com>
2024-12-15 15:19:10 +1000

    done up to 43, only 46 left now

---
+++ 34/PSTS_34_148_181/checker.py
@@ -0,0 +1,37 @@
+from helper_methods import * 
+import importlib
+import sys 
+
+importlib.reload(sys.modules['helper_methods'])
+
+n = 34 
+
+point_set = list(range(n))
+
+starter_triples = txt_to_list_of_triples('34/PSTS_34_148_181/data/starter_triples.txt', str_to_int=True)
+
+A1_triples = []
+for i in range(n-1): 
+    for T in starter_triples: 
+        A1_triples.append(sorted([(T[j]+i)%(n-1) for j in range(len(T))]))
+
+# we represent infty with 33 for compatibility with sorted() 
+A2_triples = [sorted([33, 2*i+1, 2*i+2]) for i in range(int((n-2)/2))]
+
+triples = A1_triples + A2_triples
+
+is_PSTS_checker(triples,point_set)
+
+APCs = txt_to_dict_of_APCs('34/PSTS_34_148_181/data/APCs.txt', str_to_int=True)
+
+is_APCs_checker(APCs, point_set)
+has_APCs_checker(triples, APCs)
+
+extra_starter_triples = txt_to_list_of_triples('34/PSTS_34_148_181/data/extra_starter_triples.txt', str_to_int=True)
+
+extra_triples = [] 
+for i in range(n-1): 
+    for T in extra_starter_triples: 
+        extra_triples.append(sorted([(T[j]+i)%(n-1) for j in range(len(T))]))
+
+extra_triples_checker(triples, extra_triples, point_set)
--- 43/PSTS_43_70_98/checker.py
+++ 43/PSTS_43_70_98/checker.py
@@ -4,7 +4,7 @@ import sys
 
 importlib.reload(sys.modules['helper_methods'])
 
-n = 34
+n = 43
 m = int((n-1)/3)
 
 point_set, triples = three_rotational_constructor(m)
@@ -14,6 +14,6 @@ APCs = three_rotational_APC_constructor(m)
 is_APCs_checker(APCs, point_set)
 has_APCs_checker(triples, APCs)
 
-extra_starter_triples = txt_to_list_of_triples('34/PSTS_34_55_176/data/extra_starter_triples.txt')
+extra_starter_triples = txt_to_list_of_triples('43/PSTS_43_70_98/data/extra_starter_triples.txt')
 extra_triples = three_rotational_extra_triple_constructor(m,extra_starter_triples)
 extra_triples_checker(triples, extra_triples, point_set)
