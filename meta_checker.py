26d780262055ef249c98cb2078b3dcd1906133fd
= <JackNeubecker.13@gmail.com>
2024-12-15 16:46:52 +1000

    updated meta checker

--- meta_checker.py
+++ meta_checker.py
@@ -1,3 +1,39 @@
 import PSTS_13_26_26.checker
+import PSTS_16_29_35.checker
+import PSTS_16_34_37.checker
+import PSTS_16_37_37.checker
+import PSTS_19_36_57.checker
+import PSTS_22_35_70.checker
+import PSTS_22_63_73.checker
+import PSTS_25_040_052.checker
+import PSTS_25_050_100.checker
+import PSTS_28_045_120.checker
+import PSTS_28_121_121.checker
+import PSTS_31_050_070.checker
+import PSTS_31_062_155.checker
+import PSTS_34_055_176.checker
+import PSTS_34_148_181.checker
+import PSTS_43_070_098.checker
+import PSTS_43_086_301.checker
+import PSTS_46_075_330.checker
+import PSTS_46_292_337.checker
 
 PSTS_13_26_26.checker.checker()
+PSTS_16_29_35.checker.checker()
+PSTS_16_34_37.checker.checker()
+PSTS_16_37_37.checker.checker()
+PSTS_19_36_57.checker.checker()
+PSTS_22_35_70.checker.checker()
+PSTS_22_63_73.checker.checker()
+PSTS_25_040_052.checker.checker()
+PSTS_25_050_100.checker.checker()
+PSTS_28_045_120.checker.checker()
+PSTS_28_121_121.checker.checker()
+PSTS_31_050_070.checker.checker()
+PSTS_31_062_155.checker.checker()
+PSTS_34_055_176.checker.checker()
+PSTS_34_148_181.checker.checker()
+PSTS_43_070_098.checker.checker()
+PSTS_43_086_301.checker.checker()
+PSTS_46_075_330.checker.checker()
+PSTS_46_292_337.checker.checker()
